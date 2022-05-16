a9b2c4e959c4243ee1bdbd15cec47764855e6da7
nkeneng <nkeneng.junior@gmail.com>
2022-05-17 01:34:34 +0200

    theme icon updated

--- assets/js/components/Home.js
+++ assets/js/components/Home.js
@@ -1,13 +1,10 @@
 import React, {Component} from 'react';
 import {AboutMe} from "./Components_Parts/Home_Components/About_Me";
-import {Hobby} from "./Components_Parts/Hobby";
 import LanguageContext from "./Context/LanguageContext";
 import Feedback from "./Components_Parts/Home_Components/Feedback";
 import Services from "./Components_Parts/Home_Components/Services";
 import dark_profil from "../../images/profil-black.png";
-import light_profil from "../../images/photo-profil.jpg";
 import NameProfession from "./Components_Parts/NameProfession";
-
 
 class Home extends Component {
     static contextType = LanguageContext;
@@ -15,6 +12,11 @@ class Home extends Component {
         const language = this.context;
         return (
             <div className="page is-12 is-12-touch  column mt-4">
+                <div className="demo-sticker">
+                    <a>
+                        <i className="fa fa-lightbulb-o" aria-hidden="true"></i>
+                    </a>
+                </div>
                 <div className={"columns is- is-hidden-desktop  my-6"}>
                     <div className="column is-full profil-image  ">
                         <div className="circular--landscape m-auto d-block">
